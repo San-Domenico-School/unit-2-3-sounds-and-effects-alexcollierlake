30edc12a75ae2c9967641a151c161af0614a7cd5
alexcollierlake <112986612+alexcollierlake@users.noreply.github.com>
2023-10-27 11:57:54 -0700

    playerController done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/**********
+ * 
+ * This script is a component of the player and controls
+ * the movement based on player input
+ * 
+ * October 17, 2023
+ * Alexandra Collier-Lake
+ * 
+ ***********/
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float jumpForce, gravityModifier;
+    [SerializeField] private ParticleSystem explosionParticle, dirtParticle;
+    [SerializeField] private AudioClip jumpSound, crashSound;
+
+    private Animator playerAnimation;
+    private AudioSource playerAudio;
+    private Rigidbody playerRb;
+    private bool isOnGround;
+    public bool gameOver { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        isOnGround = true;
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnJump(InputValue input)
+    {
+        if(isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Ground")
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //
+    }
+}
